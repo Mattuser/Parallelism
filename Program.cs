8842553aba95f5545f12564fb02cdcbcbc2f5627
Matheus Vinicius <matheus.stsousa@gmail.com>
2024-08-29 23:02:35 -0300

    Added V2_1 to the ThreadTest

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@ using System.Diagnostics;
 
 var sw = new Stopwatch();
 
-sw.Start();
-var result = Prime.InRangeV2(200, 800000);
-sw.Stop();
+//sw.Start();
+//var result = Prime.InRangeV2_1(200, 800000);
+//sw.Stop();
 
-Console.WriteLine($"{result} prime numbers found in " +
-    $"{sw.ElapsedMilliseconds / 1000} seconds " +
-    $"({Environment.ProcessorCount} processors).");
+//Console.WriteLine($"{result} prime numbers found in " +
+//    $"{sw.ElapsedMilliseconds / 1000} seconds " +
+//    $"({Environment.ProcessorCount} processors).");
 
 
-//ThreadTest.V1(1, 10);
+ThreadTest.V1_2(1, 10);
--- ThreadTest.cs
+++ ThreadTest.cs
@@ -28,4 +28,38 @@ public class ThreadTest
         }
 
     }
+
+    public static void V1_2(int start, int end)
+    {
+        var range = end - start;
+        var numberOfThreads = (long)Environment.ProcessorCount;
+        var results = new long[numberOfThreads];
+
+        var threads = new Thread[numberOfThreads];
+        var chunkSize = range / numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            var chunkStart = start + i * chunkSize;
+            var chunkEnd = (i == (numberOfThreads - 1)) ? end : chunkStart + chunkSize;
+            var current = i;
+
+            
+            threads[i] = new Thread(() =>
+            {
+                Console.WriteLine(i);
+                results[current] = 0;
+             
+                for (var number = chunkStart; number < chunkEnd; number++)
+                {
+                    //Console.WriteLine(threads[results[current]]);
+                    results[current]++;
+                }
+
+            });
+
+            threads[i].Start();
+        }
+
+    }
 }
