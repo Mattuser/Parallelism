d133adc77e4097cd4065fdbeca057872ff09a5d2
Matheus Vinicius <matheus.stsousa@gmail.com>
2024-08-29 22:43:52 -0300

    Organizado as versões dos ranges abaixo da outra e adicionado InRange2_1 sem threadlock

--- Prime.cs
+++ Prime.cs
@@ -1,6 +1,18 @@
 ﻿namespace Parallelism;
 public static class Prime
 {
+    private static bool IsPrime(long number)
+    {
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+        for (long divisor = 3; divisor < (number / 2); divisor += 2)
+        {
+            if (number % divisor == 0) return false;
+        }
+
+        return true;
+    }
+
     #region V1
     public static long InRangeV1(long start, long end)
     {
@@ -15,17 +27,6 @@ public static class Prime
     }
     #endregion
 
-    private static bool IsPrime(long number)
-    {
-        if (number == 2) return true;
-        if (number % 2 == 0) return false;
-        for (long divisor = 3; divisor < (number / 2); divisor += 2)
-        {
-            if(number % divisor == 0) return false;
-        }
-
-        return true;
-    }
 
     #region V2
     public static long InRangeV2(long start, long end)
@@ -67,6 +68,47 @@ public static class Prime
         }
 
         return result;
+    }
+
+    #endregion
+
+    #region V3
+    public static long InRangeV2_1(long start, long end)
+    {
+        var range = end - start;
+        var numberOfThreads = (long)Environment.ProcessorCount;
+
+        var threads = new Thread[numberOfThreads];
+        var results = new long[numberOfThreads];
+
+        var chunkSize = range / numberOfThreads;
+
+        for(long i = 0; i < numberOfThreads; i++)
+        {
+            var chunkStart = start + i * chunkSize;
+            var chunkEnd = i == (numberOfThreads - 1) ? end : chunkStart + chunkSize;
+            var current = i;
+
+            threads[i] = new Thread(() =>
+            {
+                results[current] = 0;
+                for(var number = chunkStart; number < chunkEnd; number++)
+                {
+                    if (IsPrime(number))
+                        results[current]++;
+                }
+            });
+
+            threads[i].Start();
+        }
+
+        foreach(var thread in threads)
+        {
+            thread.Join();
+        }
+
+        return results.Sum();
+
     }
 
     #endregion
